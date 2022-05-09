c1bfb8d810014caeb8d5920967ae7b85bf85e606
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-05-09 16:50:38 +0200

    enrico's script push

--- 2.1 MCMinCut/MCMinCut.py
+++ 2.1 MCMinCut/MCMinCut.py
@@ -1,3 +1,12 @@
-# Genera il grafo di Fritsch (che trovi nelle note) con 9 vertici e 21 archi e verifica la frequenza empirica con la quale ottieni un taglio minimo applicando MCMinCut 105 volte. Confronta il risultato ottenuto con la stima p ≈ 2/n^2.
+# anno scorso: 
+# Genera il grafo di Fritsch (che trovi nelle note) con 9 vertici e 21 archi e 
+# verifica la frequenza empirica con la quale ottieni un taglio minimo applicando 
+# MCMinCut 10^5 volte. Confronta il risultato ottenuto con la stima p ≈ 2/n^2.
+
+# 2022:
+# Genera il grafo di Fritsch (che trovi nelle note) con 9 vertici e 21 archi e 
+# verifica la frequenza empirica ^p con la quale ottieni un taglio minimo applicando 
+# MCMinCut 10^5 volte. --- Utilizza ^p per calcolare il numero di run R necessari per 
+# ottenere il taglio minimo con una probabilità del 99,9%
 
 from random import choice
